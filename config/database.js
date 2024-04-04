0a6246cc9efb80555806d94e04ae85d4ebfba2d0
小新 <782446723@qq.com>
2024-04-04 19:05:44 +0800

    fix: 删除时间戳，同步表名

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 
 // 创建 Sequelize 实例
 const sequelize = new Sequelize('achs', 'root', '122600', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+        timestamps: false, // 不自动添加时间戳
+        freezeTableName: true // 使用单数表名
+    }
 });
 
 module.exports = sequelize;
