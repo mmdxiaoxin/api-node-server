f52ba456a70bbfa33a658618ccd5b2ddb24e4129
小新 <782446723@qq.com>
2024-04-04 19:56:56 +0800

    fix: 完善数据库表

--- models/api_category.js
+++ models/api_category.js
@@ -8,7 +8,15 @@ const ApiCategory = sequelize.define('api_category', {
         autoIncrement: true
     },
     project_id: DataTypes.BIGINT,
-    category_name: DataTypes.STRING
+    category_name: DataTypes.STRING,
+    parent_id: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        references: {
+            model: 'api_category',
+            key: 'id'
+        }
+    }
 });
 
 module.exports = ApiCategory;
