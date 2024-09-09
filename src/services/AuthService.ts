82f1ad328e6f2438588e313704381710d19f63e3
小新 <782446723@qq.com>
2024-09-10 01:14:06 +0800

    feat: 临时使用简单注册

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -5,36 +5,24 @@ import sequelize from '../config/database';
 
 class AuthService {
     // 用户注册服务
-    public async register(email: string, password: string) {
+    public async register(username: string, password: string) {
         const transaction = await sequelize.transaction();
 
         try {
             // 检查用户是否存在
-            const existingUser = await User.findOne({ where: { email }, transaction });
+            const existingUser = await User.findOne({ where: { username }, transaction });
             if (existingUser) {
-                throw new Error('该邮箱已经被用户绑定!');
+                throw new Error('该用户名已经被用户绑定!');
             }
-
-            // 哈希密码
-            const saltRounds = 10;
-            const hashedPassword = await bcrypt.hash(password, saltRounds);
 
             // 创建用户
             const newUser = await User.create(
                 {
-                    name: 'Temp User',
-                    email,
-                    password_hash: hashedPassword,
+                    username,
+                    password,
                 },
                 { transaction }
             );
-
-            // 使用用户 id 唯一化 name
-            const uniqueName = `User_${newUser.id}`;
-
-            // 更新用户 name 字段
-            newUser.name = uniqueName;
-            await newUser.save({ transaction });
 
             await transaction.commit();
             return newUser;
